9f728081f639dc9a9719719f8aa46e1a6417cc1c
Vladimir Stepanov <vladimir-stepanov-home@yandex.ru>
2024-02-07 00:00:48 +0300

    Добавлена активность LoginActivity

---
+++ app/src/main/java/com/example/onlinestoretest/LoginActivity.kt
@@ -0,0 +1,22 @@
+package com.example.onlinestoretest
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var loginButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        loginButton = findViewById(R.id.login_button)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
